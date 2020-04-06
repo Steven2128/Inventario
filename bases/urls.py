deffe7d1e75d14024b3a9b2f08bdc408dfab0fb0
Stephen Alarcón <slj212885@gmail.com>
2020-04-06 01:12:42 -0500

    Ruta login

--- bases/urls.py
+++ bases/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.Home.as_view()),
+    path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
 ]
