672187d56c854907ae348a8fffaeb69470e7734e
Stephen Alarcón <slj212885@gmail.com>
2020-04-07 15:22:14 -0500

    Categoría - Modelo

--- inv/models.py
+++ inv/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from bases.models import ClaseModelo
 
-# Create your models here.
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(max_length=100, help_text='Descripción de la categoría', unique=True)
+
+
+    def __str__(self):
+        return self.descripcion
+
+    class Meta:
+        verbose_name_plural = 'Categorias'
