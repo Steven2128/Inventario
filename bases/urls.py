cf4f5f27d9562dad3b27ecc5f03caa7c05ead1b3
Stephen Alarcón <slj212885@gmail.com>
2020-04-09 00:42:30 -0500

    Categoria - Opción de menú

--- bases/urls.py
+++ bases/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view()),
+    path('', views.Home.as_view(), name="home"),
     path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
