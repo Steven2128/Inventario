14415081a15f8b7c45dc3b52e64998b04e74d426
Stephen Alarcón <slj212885@gmail.com>
2020-04-09 01:25:04 -0500

    Categoria - New - Vista

--- inv/views.py
+++ inv/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Categoria
+from .forms import CategoriaForm
+
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
     model = Categoria
     template_name = 'inv/categoria_list.html'
     context_object_name = 'obj'
     login_url = 'login'
+
+
+class categoriaNew(LoginRequiredMixin, generic.CreateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_name = 'obj'
+    form_class = CategoriaForm
+    succes_url = reverse_lazy('categoria_list')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.usuario_creacion = self.request.user
+        return super().form_valid(form)
