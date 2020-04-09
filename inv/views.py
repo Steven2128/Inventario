14aef7f6d658160fa953378ffd7519526191f1c0
Stephen Alarcón <slj212885@gmail.com>
2020-04-09 00:18:27 -0500

    Categoria - vista - Get

--- inv/views.py
+++ inv/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Categoria
 
-# Create your views here.
+class CategoriaView(LoginRequiredMixin, generic.ListView):
+    model = Categoria
+    template_name = 'inv/categoria_list.html'
+    context_object_name = 'obj'
+    login_url = 'login'
