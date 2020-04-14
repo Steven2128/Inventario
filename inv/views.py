e0e460c852246cb00f7ed45f8e86a26b1fb7ac0a
Stephen Alarcón <slj212885@gmail.com>
2020-04-14 17:52:05 -0500

    SubCategoria - Get - Vista

--- inv/views.py
+++ inv/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Categoria
+from .models import Categoria, SubCategoria
 from .forms import CategoriaForm
 
 
@@ -42,4 +42,11 @@ class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
     template_name = 'inv/categoria_del.html'
     context_object_name = 'obj'
     success_url = reverse_lazy('categoria_list')
+    login_url = 'login'
+
+
+class SubCategoriaView(LoginRequiredMixin, generic.ListView):
+    model = SubCategoria
+    template_name = 'inv/subcategoria_list.html'
+    context_object_name = 'obj'
     login_url = 'login'
