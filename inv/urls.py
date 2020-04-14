ebc11d62b57204d00422609ea77c8d275f37492c
Stephen Alarcón <slj212885@gmail.com>
2020-04-14 17:57:18 -0500

    SubCategoria - Get - Rutas

--- inv/urls.py
+++ inv/urls.py
@@ -3,8 +3,11 @@ from django.urls import reverse_lazy
 from . import views
 from .models import Categoria
 urlpatterns = [
+    #Categorías CRUD
     path('categorias/', views.CategoriaView.as_view(), name="categoria_list"),
     path('categorias/new', views.CategoriaNew.as_view(model=Categoria, success_url=reverse_lazy('categoria_list')), name="categoria_new"),
     path('categorias/edit/<int:pk>', views.CategoriaEdit.as_view(), name="categoria_edit"),
     path('categorias/delete/<int:pk>', views.CategoriaDel.as_view(), name="categoria_del"),
+    #Subcategorías CRUD
+    path('subcategorias/', views.SubCategoriaView.as_view(), name="subcategoria_list"),
 ]
