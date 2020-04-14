ef5e254b232945d07fa3e10f01418312bb0844de
Stephen Alarcón <slj212885@gmail.com>
2020-04-13 23:37:33 -0500

    Categoria - Edit - Rutas

--- inv/urls.py
+++ inv/urls.py
@@ -4,5 +4,6 @@ from . import views
 from .models import Categoria
 urlpatterns = [
     path('categorias/', views.CategoriaView.as_view(), name="categoria_list"),
-    path('categorias/new', views.categoriaNew.as_view(model=Categoria, success_url=reverse_lazy('categoria_list')), name="categoria_new"),
+    path('categorias/new', views.CategoriaNew.as_view(model=Categoria, success_url=reverse_lazy('categoria_list')), name="categoria_new"),
+    path('categorias/edit/<int:pk>', views.CategoriaEdit.as_view(), name="categoria_edit"),
 ]
