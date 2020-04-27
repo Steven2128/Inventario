feb7f786bbdef77b102731a1b87b16645bbc7889
Stephen Alarcón <slj212885@gmail.com>
2020-04-26 23:33:57 -0500

    Modelo Cliente

--- factura/models.py
+++ factura/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from bases.models import ClaseModelo
 
-# Create your models here.
+class Cliente(ClaseModelo):
+    NAT='Natural'
+    JUR='Jurídica'
+    TIPO_CLIENTE = [
+        (NAT, 'Natural'),
+        (JUR, 'Jurídica')
+    ]
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20, null=True, blank=True)
+    tipo = models.CharField(max_length=10, choices=TIPO_CLIENTE, default=NAT)
+
+    def __str__(self):
+        return '{} {}'.format(self.nombres, self.apellidos)
+
+    def save(self):
+        self.nombres = self.nombres.upper()
+        self.apellidos = self.apellidos.upper()
+        super(Cliente, self).save()
+
+    class Meta:
+        verbose_name_plural = 'Clientes'
