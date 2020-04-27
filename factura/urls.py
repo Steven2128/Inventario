d365450e4abb6986fb7fa6554c5af3b64747d863
Stephen Alarcón <slj212885@gmail.com>
2020-04-26 23:50:37 -0500

    Listar Cliente

--- factura/urls.py
+++ factura/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('Clientes/', views.ClienteView.as_view(), name="cliente_list"),
 ]
--- factura/views.py
+++ factura/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from .models import Cliente
 
-# Create your views here.
+class ClienteView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    permission_required = 'factura.view_cliente'
+    model = Cliente
+    template_name = 'cmp/cliente_list.html'
+    context_object_name = 'obj'
