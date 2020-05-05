fe69a76a98193c9ce2372e184779628b5b4280af
Stephen Alarcón <slj212885@gmail.com>
2020-05-05 07:45:22 -0500

    Nueva Clase Modelo - Mejora usuario crea y modifica

--- bases/models.py
+++ bases/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_userforeignkey.models.fields import UserForeignKey
 
 class ClaseModelo(models.Model):
     estado = models.BooleanField(default=True)
@@ -11,4 +12,13 @@ class ClaseModelo(models.Model):
     class Meta:
         abstract = True
 
-        
+
+class ClaseModelo2(models.Model):
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateField(auto_now=True)
+    usuario_creacion = UserForeignKey(auto_user_add=True, related_name='+')
+    usuario_modificacion = UserForeignKey(auto_user=True, related_name='+')
+
+    class Meta:
+        abstract = True
