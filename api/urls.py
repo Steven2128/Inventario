3827a328de0a9f2753320c28e824f1e4323958f9
Stephen Alarcón <slj212885@gmail.com>
2020-05-12 09:53:32 -0500

    EndPoint productos - view

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('v1/productos/', views.ProductoList.as_view(), name='producto_list'),
+    path('v1/productos/<str:codigo>', views.ProductoDetalle.as_view(), name='producto_detalle'),
 ]
